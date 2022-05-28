186a0ee07c15c211693ed0f7564bd16ad1c4b804
MightyNawaf <MightyNawaf@gmail.com>
2022-05-28 15:57:08 +0300

    4rth

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              '3rd test using ci/cd',
+              '4th test using ci/cd',
             ),
             Text(
               '$_counter',
