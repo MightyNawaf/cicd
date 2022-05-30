9167cfc8440c1f9a768ff9b5f159019b1e1d554d
MightyNawaf <MightyNawaf@gmail.com>
2022-05-30 11:51:16 +0300

    Preview test

--- apps/khotta/lib/main.dart
+++ apps/khotta/lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'c',
+              'Preview Test',
             ),
             Text(
               '$_counter',
